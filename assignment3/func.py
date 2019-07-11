2d7a8ef47dc3d1e918d51862a0d3dbc2dc49bbe5
Thitisorn Pawasith <s5902041630054@Macmini-213-25.local>
2019-07-11 14:39:28 +0700

    add file for assignment3

---
+++ assignment3/func.py
@@ -0,0 +1,6 @@
+def return_42():
+  return 45;
+
+def my_method(a, b):
+  return a * b;
+
---
+++ assignment3/main.py
@@ -0,0 +1,4 @@
+from func import *
+print("Result of return_42() is ", return 42());
+
+print("Result of 5 x 6 is ", my_method(5, 6)); 
